037a062f9a2a78ed25f0a618ce2fb802a1136121
dandzu7 <166304612+dandzu7@users.noreply.github.com>
2024-04-09 14:32:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
-var api = "https://node-02.qwertycoin.org/sslnode";
+var api = "http://139.180.184.63:8197/";
 var apiList = [
-	"https://node-00.qwertycoin.org/sslnode",
-	"https://node-01.qwertycoin.org/sslnode",
-	"https://node-02.qwertycoin.org/sslnode",
-	"https://node-03.qwertycoin.org/sslnode",
-	"https://node-04.qwertycoin.org/sslnode",
-	"https://node-06.qwertycoin.org/sslnode"
-];
-var blockTargetInterval = 120;
+	"http://139.180.184.63:8197/"
+	];
+var blockTargetInterval = 60;
 var coinUnits = 100000000;
 var symbol = 'QWC';
 var refreshDelay = 30000;
@@ -24,14 +19,6 @@ var poolsStat =
     ];
 var nodesStat = 
 	[
-		["node-00.qwertycoin.org:8197"],
-		["node-01.qwertycoin.org:8197"],
-		["node-02.qwertycoin.org:8197"],
-		["node-03.qwertycoin.org:8197"],
-		["node-04.qwertycoin.org:8197"],
-		["node-05.qwertycoin.org:8197"],
-		["node-06.qwertycoin.org:8197"],
-		["node-07.qwertycoin.org:8197"],
-		["node-08.qwertycoin.org:8197"],
-		["node-09.qwertycoin.org:8197"]
-    ];
+		["139.180.184.63:8197"],
+		["66.42.60.46:8197"]
+		    ];
